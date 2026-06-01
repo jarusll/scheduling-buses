44ad5120f49e810f0f943f9c8c7b48c4f4d23e44
Suraj Yadav <26197553+jarusll@users.noreply.github.com>
2026-06-01 17:19:36 +0530

    Handle ChargerFreed

--- scheduler.py
+++ scheduler.py
@@ -1,6 +1,6 @@
 import heapq
 from dataclasses import dataclass
-from world import World, Bus, Action, Skip, Charge
+from world import World, Bus, Action, Skip, Charge, Vacant, Occupied, Charging, Driving, Waiting, Finished
 
 
 @dataclass
@@ -83,15 +83,47 @@ class Scheduler:
 
     def seed(self):
         for bus in self.state.world.buses.values():
-            self.state.events.push(bus.departure_time, BusArrived(bus.route[0], bus.bus_id))
+            first_stop = self.state.world.routes[bus.route][0]
+            self.state.events.push(bus.departure_time, BusArrived(first_stop, bus.bus_id))
         for sid, stop in self.state.world.stops.items():
             for cid in range(len(stop.chargers)):
                 self.state.events.push(0, ChargerFreed(sid, cid))
 
     def run(self):
+        world = self.state.world
+        config = world.config
+
         while self.state.events:
             event = self.state.events.pop()
-            _ = event
+            now = event.time
+
+            match event.payload:
+                case BusArrived(stop=sid, bus_id=bid):
+                    pass
+
+                case ChargerFreed(stop=sid, charger_id=cid):
+                    stop = world.stops[sid]
+                    charger = stop.chargers[cid]
+                    match charger.status:
+                        case Occupied(bus_id=bid):
+                            bus = world.buses[bid]
+                            next_sid = world.next_stop(bus.route, bus.route_index)
+                            if next_sid is None:
+                                bus.status = Finished()
+                            else:
+                                dist = world.distance(sid, next_sid)
+                                travel = int(dist / config.speed_kmph * 3600)
+                                bus.status = Driving(from_stop=sid, to_stop=next_sid, remaining_km=dist)
+                                bus.route_index += 1
+                                bus.km_remaining -= dist
+                                self.state.events.push(now + travel, BusArrived(next_sid, bid))
+                    charger.status = Vacant()
+                    if stop.queue:
+                        bus = stop.queue.pop(0)
+                        bus.status = Charging(at_stop=sid, charger_id=cid)
+                        bus.km_remaining = config.battery_range_km
+                        charger.status = Occupied(bus.bus_id, now + config.charge_time_s)
+                        self.state.events.push(now + config.charge_time_s, ChargerFreed(sid, cid))
 
     def results(self) -> dict[str, Bus]:
         return self.state.world.buses
